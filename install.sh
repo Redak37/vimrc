1cdc06af920e2318ef1271f8fca7a544f59d2cd9
Redak37 <39886519+Redak37@users.noreply.github.com>
2022-10-19 19:00:30 +0200

    added nerdtree to installation

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ mv ./vimrc/.vimrc ~/
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-
-cd ~/
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
