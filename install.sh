c4d15a3bbec5ebb86ceb2a1ecb862469a5e9bcd7
Redak37 <39886519+Redak37@users.noreply.github.com>
2022-10-19 18:13:52 +0200

    make install more user friendly
    rename file + updating script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+cd ./vimrc && mv .vimrc ~/
+
+cd ..
+
+rm -rf vimrc
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
