f602a3f2abd7fc04041466583259dd6c4b72d70f
Redak37 <39886519+Redak37@users.noreply.github.com>
2022-10-19 18:35:17 +0200

    repair error
    deleting install fodler from script wasn't actually possible

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 mv ./vimrc/.vimrc ~/
 
-rm -rf /vimrc
-
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
